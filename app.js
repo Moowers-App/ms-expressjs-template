8d349bce7115466cbd4b748a78856e4bfe5c7686
Edgar Yerena <edgar.yerena@raweng.com>
2020-02-27 15:43:26 -0600

    Created basic routing and also adding jsonwebtoken package

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 'use strict'
+
 const express = require('express')
+const helmet = require('helmet')
+const apiRoutes = require('./src/routes')
 
 const app = express()
+app.use(helmet())
+
+app.use('/api/v1', apiRoutes)
 
 // Set listeting port
 const port = process.env.PORT || 3000
